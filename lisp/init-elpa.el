af866c78e157496887a9731e5c3fb27427183663
Yumin Xia <me@yumin-xia.com>
2023-06-07 17:39:42 -0700

    add basic copilot setup

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -160,7 +160,9 @@
     toml-mode
     markdown-mode ;; indirected dep by cargo
     solidity-mode
-    solidity-flycheck)
+    solidity-flycheck
+    quelpa
+    quelpa-use-package)
   "Packages to install from melpa-unstable.")
 
 (defvar melpa-stable-banned-packages
@@ -558,5 +560,9 @@ If NO-REFRESH is nil, `package-refresh-contents' is called."
 
 ;; string
 (require-package 'string-inflection)
+
+;; package manager
+(require-package 'quelpa)
+(require-package 'quelpa-use-package)
 
 (provide 'init-elpa)
